f1ae6ece7645090e52229d186ea3d35dc3a576c7
keshiagabali <150538341+keshiagabali@users.noreply.github.com>
2024-01-21 23:17:18 +0100

    Ajout des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +18,32 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentSlideIndex = 0;
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const bannerImage = document.querySelector('.banner-img');
+const bannerText = document.querySelector('#banner p');
+
+function updateCarousel() {
+	bannerImage.src = `./assets/images/slideshow/${slides[currentSlideIndex].image}`;
+	bannerText.innerHTML = slides[currentSlideIndex].tagLine;
+
+// Event Listeners sur les flèches 
+arrowLeft.addEventListener('click', function() {
+    currentSlideIndex--;
+    if (currentSlideIndex < 0) {
+      currentSlideIndex = slides.length - 1;
+    }
+    updateCarousel();
+});
+
+arrowRight.addEventListener('click', function() {
+    currentSlideIndex++;
+    if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+}
+updateCarousel();
+});
+
